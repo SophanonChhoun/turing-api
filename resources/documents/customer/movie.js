51117cc15240f97c51fa6ecbb8d99b9b94d6e09e
SophanonChhoun <sophanon.chhoun@niptict.edu.kh>
2021-08-09 10:31:25 +0700

    [IMP]screening create

--- resources/documents/customer/movie.js
+++ resources/documents/customer/movie.js
@@ -245,35 +245,59 @@
  *
  * @apiSuccessExample  Response (example):
  HTTP/1.1 200 Success Request
- [
  {
-        "id": 4,
-        "poster": "/wHYq4CXe2oSlF3BlxFeFgb8XUW9.jpg",
-        "backdrop": "/1UPzHDjUT40abGPPr4wKxdrX0ar.jpg",
-        "title": "Amarsi un po'...",
-        "trailerUrl": "VanCRj8eI9o",
-        "synopsis": "In an accident the young mechanic Marco Coccia meets Cristiana. The two fall in love with one another. Cristiana confesses only later that she is a genuine princess. For Marco it is not a problem, but for Cristiana's parents it is a very big problem. The two must separate. When one evening Cristiana calls Marco and lets him know about her forthcoming wedding, Marco with the car races toward Paris and has an accident...",
-        "runningTime": 93,
-        "directors": [
-            {
-                "id": 1,
-                "name": "Neath Mony"
-            }
-        ],
-        "casts": [
-            {
-                "id": 1,
-                "name": "Neath Mony"
-            }
-        ],
-        "genres": [
-            {
-                "id": 1,
-                "name": "Action"
-            }
-        ]
-    }
- ]
+    "id": 4,
+    "title": "Amarsi un po'...",
+    "synopsis": "In an accident the young mechanic Marco Coccia meets Cristiana. The two fall in love with one another. Cristiana confesses only later that she is a genuine princess. For Marco it is not a problem, but for Cristiana's parents it is a very big problem. The two must separate. When one evening Cristiana calls Marco and lets him know about her forthcoming wedding, Marco with the car races toward Paris and has an accident...",
+    "rated": "R",
+    "directors": [
+        {
+            "id": 1,
+            "name": "Neath Mony"
+        }
+    ],
+    "casts": [
+        {
+            "id": 1,
+            "name": "Neath Mony"
+        }
+    ],
+    "genres": [
+        {
+            "id": 1,
+            "name": "Action"
+        }
+    ],
+    "poster": "/wHYq4CXe2oSlF3BlxFeFgb8XUW9.jpg",
+    "backdrop": "/1UPzHDjUT40abGPPr4wKxdrX0ar.jpg",
+    "trailerUrl": "VanCRj8eI9o",
+    "cinemas": [
+        {
+            "id": 2,
+            "name": "Turing Niptict",
+            "location": "Niptict",
+            "screening": [
+                [
+                    {
+                        "id": 7,
+                        "movieId": 4,
+                        "price": 10,
+                        "theaterId": 2,
+                        "date": "2021-08-09",
+                        "start_time": "09:33:00",
+                        "status": true,
+                        "created_at": "2021-08-07T02:34:11.000000Z",
+                        "updated_at": "2021-08-09T03:24:37.000000Z",
+                        "cinemaId": 2,
+                        "subId": 1,
+                        "dubId": 2,
+                        "deleted_at": null
+                    }
+                ]
+            ]
+        }
+    ]
+ }
  *
  * @apiUse NotFound
  * @apiUse ServerServerError
