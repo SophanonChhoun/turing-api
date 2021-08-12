0e96310f994e7f50e0d3a8212f43203abd3fec96
SophanonChhoun <sophanon.chhoun@niptict.edu.kh>
2021-08-12 21:35:44 +0700

    [IMP]remove class handlecors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,6 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreflightResponse::class,
-        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
